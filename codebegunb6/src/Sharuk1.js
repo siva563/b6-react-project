ed0da67a3b656297c8f69c456749f14bdada25b7
Shaiksharuk1234 <shaikshaiksharuk954@gmail.com>
2025-01-08 20:05:28 +0530

    push my code

---
+++ codebegunb6/src/Sharuk1.js
@@ -0,0 +1,11 @@
+import React from 'react';
+function Sharuk1(){
+    return(
+        <div>
+            <h2>
+                welcome to compomets
+            </h2>
+        </div>
+    )
+}
+export default Sharuk1;
