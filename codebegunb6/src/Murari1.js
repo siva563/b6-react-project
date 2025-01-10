c8f793e50b00652559ec02c065a1ef2062ac405a
murariraj <murariraj2002@gmail.com>
2025-01-10 13:48:28 +0530

    Added : (border,width,backgroundcolour,font,height and padding)

--- codebegunb6/src/Murari1.js
+++ codebegunb6/src/Murari1.js
@@ -1,8 +1,16 @@
 import React from 'react';
 function Murari1() {
     return (
-        <div>
-            <h2> 5-Murari</h2>
+        <div style={{
+            border: " 1px solid black ",
+            width: "20%",
+            backgroundColor: "yellow",
+            fontFamily: "cursive",
+            height: "50px",
+            paddingBottom: "25px"
+
+        }}>
+            <h2 > 5-Murari</h2>
         </div>
     )
 }
