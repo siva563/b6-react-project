3bb822fee83b2da8503ead613a27a2bcabfd69d3
Mangolupriya <mangolu.priya2002@gmail.com>
2025-01-10 23:38:35 +0530

    priya component style

--- codebegunb6/src/Priya.js
+++ codebegunb6/src/Priya.js
@@ -2,10 +2,16 @@ import React from 'react';
 
 function Priya() {
     return (
-      <div className="App">
-        <h2>6 Priya</h2>
-  
-      </div>
+      <div className="App" style={{ textAlign: "left" }}>
+      <h2 style={{ color: "black",
+        backgroundColor: "lightblue",
+        border: "2px solid black",
+        padding: "7px",
+        fontFamily: "Algerian", 
+        fontSize: "20px", 
+        display: "inline-block" }} > 6.Priya </h2>
+
+    </div>
     );
   }
   
