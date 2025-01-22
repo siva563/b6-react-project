239e0354b0c4df17d4862cce85af9916b7ba0319
Murali-2009 <muralijanupu1996@gmail.com>
2025-01-22 19:40:53 +0530

    i changed tesxt center to left and added background color

--- codebegunb6/src/Murali.js
+++ codebegunb6/src/Murali.js
@@ -2,10 +2,19 @@ import React from 'react';
 
 function Murali() {
    return (
-     <div className="App">
+    <div style={{
+      border: " 1px solid black ",
+      width: "20%",
+      backgroundColor: "blue",
+      fontFamily: "sans-serif",
+      height: "50px",
+      paddingBottom: "25px"
+
+    }}>
        <h2>4.Murali</h2>
      </div>
    );
+
  }
  
  export default Murali;
