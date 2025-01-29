51a39c4c7d83dbcd74b1f9ed0cbd03a07fe644c2
Mangolupriya <mangolu.priya2002@gmail.com>
2025-01-29 20:01:36 +0530

    changed form priya

--- codebegunb6/src/Priya_form.js
+++ codebegunb6/src/Priya_form.js
@@ -4,49 +4,57 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Priya_form(){
     return(
-      <div className="container mt-5">
+      <div className="container mt-15">
       <h1>Create Student</h1>
 
-      <div className="border p-3" style={{ height: "500px" }}>
+      <div className="border p-3" style={{ height: "450px" }}>
 
           
           <div className="row align-items-end">
 
-  <div class="col-md-5">
+  <div class="col-md-6">
     <label for="input" class="form-label" style={{ float: "left" }}>Name</label>
-    <input type="text" class="form-control" id="name" placeholder="Yourname" />
+    <input type="text" class="form-control" id="name" placeholder="Your name" />
   </div>
-  <div class="col-md-5">
-    <label for="inputPassword4" class="form-label" style={{ float: "left" }}>email</label>
-    <input type="email" class="form-control" id="email" placeholder="your mail"/>
+  <div class="col-md-6">
+    <label for="inputPassword4" class="form-label" style={{ float: "left" }}>Email</label>
+    <input type="email" class="form-control" id="email" placeholder="your email"/>
   </div>
   <div className="col-md-12 ">
                         <label className="form-label" style={{ float: "left" }}>Biographical Statement</label>
                         <input type="text" className="form-control " placeholder="A bit about you" style={{ height: "80px", width: "100%" }} />
                     </div>
 
-       <br/>
+       
 <div class="col-md-4">
     <label for="inputState" class="form-label" style={{ float: "left" }}>Level</label>
     <select id="inputState" class="form-select">
-      <option selected>Level</option>
+      <option selected>Fresherman</option>
       <option>Fresher</option>
       <option>Experience </option>
     </select>
+    <div className="col-md-6" style={{ float: "left" }}>
+                            <p style={{ color: "red" }}>What is your level? required</p>
+  </div>
   </div>
   <div class="col-md-4">
     <label style={{ float :"left"}}>CGPA</label>
-    <input type="text" class="form-control" id="inputEmail4" />
-  </div>
+    <input type="text" class="form-control" id="cgpa" />
+    <div className="col-md-6" style={{ float: "left" }}>
+                            <p style={{ color: "red" }}>Select one (required)</p>
+                        </div>
 
+  </div>
+  
   <div className="col-md-4">
                         <label className="form-label" style={{ float: "left" }}>Date Erolled</label>
                         <input type="date" className="form-control " />
                     </div>
+                  
 
 
 <div>
-<label style={{ float :"left"}}>Hobbies</label><br/>
+<label style={{ float :"left"}}>Hobbies</label>
 <div className ="col-md-1" style={{float:"left"}}>
   <input type="checkbox"  id="surfing" name="hobbies" value="surfing"/>
   <label htmlFor="surfing" style={{marginLeft: "5px"}}>Surfing</label>
@@ -54,52 +62,52 @@ function Priya_form(){
 
 
 <div className ="col-md-1" style={{float:"left"}}>
-  <input type="checkbox"  id="surfing" name="hobbies" value="surfing"/>
+  <input type="checkbox"  id="surfing" name="hobbies" value="runnig"/>
   <label htmlFor="surfing" style={{marginLeft: "5px"}}>Running</label>
 </div>
 
 <div className ="col-md-1" style={{float:"left"}}>
-  <input type="checkbox"  id="surfing" name="hobbies" value="surfing"/>
+  <input type="checkbox"  id="surfing" name="hobbies" value="Biking"/>
   <label htmlFor="surfing" style={{marginLeft: "5px"}}>Biking</label>
 </div>
 
 <div className ="col-md-1" style={{float:"left"}}>
-  <input type="checkbox"  id="surfing" name="hobbies" value="surfing"/>
+  <input type="checkbox"  id="surfing" name="hobbies" value="paddling"/>
   <label htmlFor="surfing" style={{marginLeft: "5px"}}>Paddling</label>
 </div>
-<div className="col-md-1" style={{ float: "left" }}>
-                            <p style={{ color: "red" }}> (optional)</p>
+<div className="col-md-3" style={{ float: "left" }}>
+                            <p style={{ color: "ash" }}>select hobbies(optional)</p>
                         </div>
 </div>
 
 
 
 <div>
-                        <label style={{ float: "left" }}>Major</label><br />
-                        <div className="col-md-1" style={{ float: "left" }}>
-                            <input type="radio" id="Physics" name="Major" value="Physics" />
-                            <label htmlFor="Physics" style={{ marginLeft: "5px" }}>Physics</label>
-                        </div>
-                        <div className="col-md-1" style={{ float: "left" }}>
-                            <input type="radio" id="Math" name="Major" value="Math" />
-                            <label htmlFor="Math" style={{ marginLeft: "5px" }}>Math</label>
-                        </div>
-                        <div className="col-md-1" style={{ float: "left" }}>
-                            <input type="radio" id="Chemistry" name="Major" value="Chemistry" />
-                            <label htmlFor="Chemistry" style={{ marginLeft: "5px" }}>Chemistry</label>
-                        </div>
-                        <div className="col-md-1" style={{ float: "left" }}>
-                            <input type="radio" id="Computer" name="Major" value="Computer" />
-                            <label htmlFor="Computer" style={{ marginLeft: "4px" }}>Computer</label>
-                        </div>
-                        <div className="col-md-1" style={{ float: "left" }}>
-                            <p style={{ color: "red" }}> (required)</p>
-                        </div>
-                    </div>
+<label style={{ float: "left" }}>Major</label>
+<div className="col-md-1" style={{ float: "left" }}>
+<input type="radio" id="Physics" name="Major" value="Physics" />
+<label htmlFor="Physics" style={{ marginLeft: "5px" }}>Physics</label>
+</div>
+<div className="col-md-1" style={{ float: "left" }}>
+<input type="radio" id="Math" name="Major" value="Math" />
+<label htmlFor="Math" style={{ marginLeft: "5px" }}>Maths</label>
+</div>
+<div className="col-md-1" style={{ float: "left" }}>
+  <input type="radio" id="Chemistry" name="Major" value="Chemistry" />
+<label htmlFor="Chemistry" style={{ marginLeft: "5px" }}>Chemistry</label>
+</div>
+<div className="col-md-2" style={{ float: "left" }}>
+<input type="radio" id="Computerscience" name="Major" value="Computerscience" />
+<label htmlFor="Computerscience" style={{ marginLeft: "6px" }}>Computer Science</label>
+</div>
+<div className="col-md-3" style={{ float: "left" }}>
+<p style={{ color: "red" }}> (what is your Major?required)</p>
+</div>
+</div>
 
-                    <div style={{ display: "flex", justifyContent: "left", alignItems: "center" }}>
-                        <button type="submit" className="btn btn-primary">Submit</button>
-                    </div>
+<div style={{ display: "flex", justifyContent: "left", alignItems: "center" }}>
+<button type="submit" className="btn btn-primary">Submit</button>
+</div>
   
        
  
