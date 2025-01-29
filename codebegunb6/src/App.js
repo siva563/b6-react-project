ce6a310173a918702df388db0f1828655133a8df
chandra-006 <craju852292@gmail.com>
2025-01-29 19:50:44 +0530

    created a form

--- codebegunb6/src/App.js
+++ codebegunb6/src/App.js
@@ -1,27 +1,24 @@
-import './App.css';
-import Sharuk1 from './Sharuk1';
-import Sathvika from './Sathvika';
-import Chandu from './Chandu';
-import Murali from './Murali';
-import Murari1 from './Murari1';
-import Priya from './Priya';
-import Snehitha from './Snehitha';
-import Formemail from './Formemail';
-import Format from './Format';
-import Text from './Text';
-import Radio from './Radio';
-import MurariForm from './MurariForm';
-import Checkbox from './Checkbox';
+// import './App.css';
+// import Sharuk1 from './Sharuk1';
+// import Sathvika from './Sathvika';
+// import Chandu from './Chandu';
+// import Murali from './Murali';
+// import Murari1 from './Murari1';
+// import Priya from './Priya';
+// import Snehitha from './Snehitha';
+// import Formemail from './Formemail';
+// import Format from './Format';
+// import Text from './Text';
+// import Radio from './Radio';
+// import Checkbox from './Checkbox';
 
-
-
-
+import Chandu_form from "./Chandu_form";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello Codebegun Welcome to React World</h1>
-      <Sharuk1 />
+      {/* <Sharuk1 />
       <Sathvika />
       <Chandu />
       <Murali />
@@ -31,13 +28,9 @@ function App() {
       <Formemail />
       <Format />
       <Text />
-    <Radio />
-      
-
-     
-      <Checkbox />
-     <MurariForm />
-
+      <Radio />
+      <Checkbox /> */}
+      <Chandu_form />
     </div>
   );
 }
